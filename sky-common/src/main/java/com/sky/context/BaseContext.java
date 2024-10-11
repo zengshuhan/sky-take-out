7192bc48a57889e62bdb6f3e08eef2d29a549def
zengshuhan <c20010621@icloud.com>
2024-10-11 17:51:08 +0800

    外卖项目初始代码

---
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -0,0 +1,19 @@
+package com.sky.context;
+//上下文
+public class BaseContext {
+
+    public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    public static void setCurrentId(Long id) {
+        threadLocal.set(id);
+    }
+
+    public static Long getCurrentId() {
+        return threadLocal.get();
+    }
+
+    public static void removeCurrentId() {
+        threadLocal.remove();
+    }
+
+}
---
+++ sky-common/src/main/java/com/sky/exception/AccountLockedException.java
@@ -0,0 +1,15 @@
+package com.sky.exception;
+//自定义的异常类
+/**
+ * 账号被锁定异常
+ */
+public class AccountLockedException extends BaseException {
+
+    public AccountLockedException() {
+    }
+
+    public AccountLockedException(String msg) {
+        super(msg);
+    }
+
+}
